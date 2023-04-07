49329208b3c8712eeb5d87c2e95bdb3ed811ca67
Amanda <amandarb43@gmail.com>
2023-04-06 21:42:12 -0700

    took API added to server.js, played with navbar and made errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {useEffect,useState} from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/navbar';
+import Home from './components/home';
+import Taskform from './components/taskform';
+import Account from './components/register';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <div className='App'>
+        <Navbar />
+          <div className='content'>
+            <Switch>
+              <Route exact path="/">
+                  <Home />
+              </Route>
+              <Route exact path="/create">
+                  <Taskform />
+              </Route>
+              <Route exact path="/register">
+                  <Account />
+              </Route>
+            </Switch>
+          </div>
     </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+// function App() {
+//   const [backendData, setBackendData] = useState([{}])
+//   useEffect(() => {
+//     fetch("/api").then(
+//         response => response.json()
+//     ).then(
+//       data => {
+//           setBackendData(data)
+//       }
+//     )
+//   }, [])
+//     return (
+//       <div>
+      
+//   {( typeof backendData.users === 'undefined')?(
+//     <p>Loading...</p>
+//   ): (
+//      backendData.users.map((user,i) => (
+//       <p key={i}>{user}</p>
+//      ))
+//   )}
+//   <Navbar />
+//   <Home />
+//       </div>
+//     )
+//   }
+  
+//   export default App
